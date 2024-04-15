1acce5e717c8ae639693b8af9b73701fdb671a3a
elamthendral <legendthendral@gmail.com>
2024-04-15 18:43:22 +0530

    Create sorted_dictionary.py

---
+++ sorted_dictionary.py
@@ -0,0 +1,20 @@
+def sort_method(value):
+    sort_keys=[]
+    sort_values=[]
+    for k,v in value.items():
+        if not sort_keys:
+            sort_keys.append(k)
+            sort_values.append(v)
+        else:
+            print(k,v)
+            item = sort_values[-1]
+            if item > v:
+                ind = sort_values.index(item)
+                sort_values.insert(ind, v)
+                sort_keys.insert(ind, k)
+            else:
+                sort_values.append(v)
+                sort_keys.append(k)
+        print(sort_keys, sort_values)
+    sort_items={sort_keys[i]:sort_values[i] for i in range(len(sort_keys))}
+    print(sort_items)
