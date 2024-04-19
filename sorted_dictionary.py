dbee38ac572124d1b899c3b54be71b0747fe7957
elamthendral <legendthendral@gmail.com>
2024-04-19 06:39:37 +0530

    Update sorted_dictionary.py

--- sorted_dictionary.py
+++ sorted_dictionary.py
@@ -18,3 +18,5 @@ def sort_method(value):
         print(sort_keys, sort_values)
     sort_items={sort_keys[i]:sort_values[i] for i in range(len(sort_keys))}
     print(sort_items)
+
+#input {'a':3, 'b':-2, 'c':1} output: {'b':-2, 'c':1, 'a':3}
