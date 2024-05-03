6d6a9f7f45f3eeb2492e926a8d948ce7e0c2b07d
elamthendral <legendthendral@gmail.com>
2024-05-03 18:59:37 +0530

    Update list_range.py

--- list_range.py
+++ list_range.py
@@ -1,3 +1,4 @@
+1)#input: [[1,3],[2,6],[8,10],[11,18]] output:[[1, 6], [8, 10], [11, 18]]
 def find_list(input):
     output_list = []
     for item in input:
@@ -18,4 +19,26 @@ def find_list(input):
     return output_list 
 
 print(find_list([[1,9],[4,6],[8,10],[11,18]]))
-#input: [[1,3],[2,6],[8,10],[11,18]] output:[[1, 6], [8, 10], [11, 18]]
+
+
+
+2) # x=[7,8,9,10,11,12,13,1,2,3,4,5,6]	 
+
+# output=[[7,8,9,10],[11,12,13,1],[2,3,4,5],[6]]
+
+def split_list(input, range=4):
+    output_list = []
+    sub_list = []
+    final_check_flag=False
+    for item in input:
+        if len(sub_list)==range:
+            output_list.append(sub_list)
+            final_check_flag=True
+            sub_list=[]
+        else:
+            final_check_flag = False
+            sub_list.append(item)
+    
+    if not final_check_flag:
+        output_list.append(sub_list)
+    return output_list
